f77119a3e98fbb9f3abfbeb0a3cb3d16cf4ef84b
zlx454545 <zlx454545@gmail.com>
2023-11-17 15:46:09 +0900

    chore: supabse 타입 설정

---
+++ src/data/data-source/api/supabaseDB/database.types.ts
@@ -0,0 +1,44 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json | undefined }
+  | Json[]
+
+export interface Database {
+  public: {
+    Tables: {
+      issues: {
+        Row: {
+          created_at: string
+          id: number
+          title: string
+        }
+        Insert: {
+          created_at?: string
+          id?: number
+          title?: string
+        }
+        Update: {
+          created_at?: string
+          id?: number
+          title?: string
+        }
+        Relationships: []
+      }
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      [_ in never]: never
+    }
+    Enums: {
+      [_ in never]: never
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
+}
--- src/data/data-source/api/supabaseDB/supabase.ts
+++ src/data/data-source/api/supabaseDB/supabase.ts
@@ -1,7 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
+import { Database } from './database.types';
 
 const supabaseUrl = import.meta.env.SUPABASE_URL;
 const supabaseKey = import.meta.env.SUPABASE_API_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey);
+
+const supabase = createClient<Database>(supabaseUrl, supabaseKey);
 
 export default supabase;
