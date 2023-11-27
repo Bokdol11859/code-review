ffbf4e07a3e2f42bc4008b5b8e019d5fd4574e77
zlx454545 <zlx454545@gmail.com>
2023-11-27 16:06:50 +0900

    feat: Table 컴포넌트 구현

---
+++ src/common-ui/table/index.tsx
@@ -0,0 +1,70 @@
+import { ReactNode, createContext, useContext } from 'react';
+
+interface TableProps {
+  columns: string;
+  children: ReactNode;
+}
+
+interface HeaderProps {
+  children: ReactNode;
+}
+
+interface BodyProps<T> {
+  data: T[] | undefined;
+  render: (item: T) => ReactNode;
+}
+
+interface RowProps {
+  children: ReactNode;
+}
+
+const TableContext = createContext<Pick<TableProps, 'columns'>>(null!);
+
+function Table({ columns, children }: TableProps) {
+  return (
+    <TableContext.Provider value={{ columns }}>
+      <div className="border border-solid border-neutral-border rounded-large overflow-hidden">
+        {children}
+      </div>
+    </TableContext.Provider>
+  );
+}
+
+function Header({ children }: HeaderProps) {
+  const { columns } = useContext(TableContext);
+
+  return (
+    <div
+      style={{ gridTemplateColumns: columns }}
+      className={`grid gap-8 py-4 px-8 bg-neutral-background border-b border-solid border-neutral-border`}
+    >
+      {children}
+    </div>
+  );
+}
+
+function Body<T>({ data, render }: BodyProps<T>) {
+  if (!data || data?.length === 0)
+    return <p className="text-center m-6">표시할 항목이 없습니다</p>;
+
+  return <div className="bg-neutral-background-strong">{data.map(render)}</div>;
+}
+
+function Row({ children }: RowProps) {
+  const { columns } = useContext(TableContext);
+
+  return (
+    <div
+      style={{ gridTemplateColumns: columns }}
+      className={`grid gap-8 py-4 px-8 border-b last:border-b-0 border-solid border-neutral-border`}
+    >
+      {children}
+    </div>
+  );
+}
+
+Table.Header = Header;
+Table.Body = Body;
+Table.Row = Row;
+
+export default Table;
