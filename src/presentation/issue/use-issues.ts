1d114fadb6a25078f1928a9ec7eb1782b400462b
zlx454545 <zlx454545@gmail.com>
2023-11-28 14:21:12 +0900

    feat: use issues 구현

---
+++ src/presentation/issue/use-issues.ts
@@ -0,0 +1,27 @@
+import IssueDataSourceImpl from '../../data/data-source/api/issue-data-source-impl';
+import { IssueRepositoryImpl } from '../../data/repository/issue-repository-impl';
+
+import { GetIssues } from '../../domain/use-case/issues/get-issues';
+import { useQuery } from '@tanstack/react-query';
+
+export default function useIssues() {
+  const issuesDataSourceImpl = new IssueDataSourceImpl();
+  const issuesRepositoryImpl = new IssueRepositoryImpl(issuesDataSourceImpl);
+
+  const getIssuesUseCase = new GetIssues(issuesRepositoryImpl);
+
+  const {
+    isLoading,
+    data: issues,
+    error,
+  } = useQuery({
+    queryKey: ['issues'],
+    queryFn: () => getIssuesUseCase.invoke(),
+  });
+
+  return {
+    isLoading,
+    issues,
+    error,
+  };
+}
