ccc043d52e610db6f3c0508b5d674faffb044fef
zlx454545 <zlx454545@gmail.com>
2023-11-28 14:22:05 +0900

    feat: issue table 구현

---
+++ src/presentation/issue/issue-table/index.tsx
@@ -0,0 +1,19 @@
+import useIssues from '../use-issues';
+import Table from '../../../common-ui/table';
+import IssueRow from './issue-row';
+import IssueHeader from './issue-header';
+
+function IssueTable() {
+  const { issues } = useIssues();
+
+  return (
+    <Table columns="1rem 1fr auto">
+      <IssueHeader />
+      <Table.Body
+        data={issues}
+        render={(issue) => <IssueRow key={issue.id} issue={issue} />}
+      />
+    </Table>
+  );
+}
+export default IssueTable;
