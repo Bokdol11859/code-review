ce72107c7235d5c9b780852a99fa587742185dc8
zlx454545 <zlx454545@gmail.com>
2023-11-28 12:50:38 +0900

    chore: react-query 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,37 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import AppLayout from './common-ui/app-layout';
+import IssueTable from './presentation/issue/issue-table';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 function AComponent() {
-  return (
-    <div className="font-bold text-neutral-text-strong bg-accent-background rounded-large text-S">
-      a 페이지
-    </div>
-  );
+  return <IssueTable />;
 }
 
 function BComponent() {
   return <div>b 페이지</div>;
 }
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 0,
+    },
+  },
+});
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<AppLayout />}>
-          <Route index element={<Navigate replace to="a" />} />
-          <Route path="a" element={<AComponent />} />
-          <Route path="b" element={<BComponent />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="a" />} />
+            <Route path="a" element={<AComponent />} />
+            <Route path="b" element={<BComponent />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 export default App;
