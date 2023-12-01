2e7be44b3adc1e0d67111c254166e7a291902dcc
zlx454545 <zlx454545@gmail.com>
2023-12-01 21:31:46 +0900

    refactor: issue 도메인 모델 수정

--- src/domain/model/issue.ts
+++ src/domain/model/issue.ts
@@ -1,6 +1,12 @@
+import { Label } from './label';
+import { MileStone } from './milestone';
+
 export interface Issue {
   id: Brand<number, Issue>;
   title: string;
+  contents: string | null;
   isOpen: boolean;
   createdAt: Date;
+  labels: Label[];
+  milestone: MileStone | null;
 }
