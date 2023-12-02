632e357a5d5e92e951ee934ceaa480f7745d9cd2
zlx454545 <zlx454545@gmail.com>
2023-12-02 14:39:24 +0900

    fix: issue api entity 타입 수정

--- src/data/data-source/api/entity/issue-api-entity.ts
+++ src/data/data-source/api/entity/issue-api-entity.ts
@@ -3,14 +3,10 @@ import { Tables } from '../supabase-db/database.types';
 interface LabelInfo
   extends Pick<
     Tables<'labels'>,
-    'id' | 'title' | 'description' | 'text_color' | 'background_color'
+    'id' | 'title' | 'text_color' | 'background_color'
   > {}
 
-interface MilestoneInfo
-  extends Pick<
-    Tables<'milestones'>,
-    'id' | 'title' | 'description' | 'due_date' | 'is_open'
-  > {}
+interface MilestoneInfo extends Pick<Tables<'milestones'>, 'id' | 'title'> {}
 
 export interface IssueAPIEntity extends Omit<Tables<'issues'>, 'milestone_id'> {
   labels: LabelInfo[];
