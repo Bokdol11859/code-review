f3332e9d9ebafee67a0b34c71735a62108b43de4
zlx454545 <zlx454545@gmail.com>
2023-12-09 21:11:00 +0900

    fix: issue table 기본 search parameter 설정

--- src/presentation/issue/issue-table/index.tsx
+++ src/presentation/issue/issue-table/index.tsx
@@ -1,12 +1,19 @@
+import { useEffect } from 'react';
 import useIssues from '../use-issues';
 import Table from '../../../common-ui/table';
 import IssueRow from './issue-row';
 import IssueHeader from './issue-header';
 import Menus from '../../../common-ui/menus';
 import { SelectedIssuesProvider } from '../SelectedIssuesContext';
+import { useNavigate } from 'react-router-dom';
 
 function IssueTable() {
   const { issues, openIssueCount, closeIssueCount } = useIssues();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate('/issues?isOpen=open', { replace: true });
+  }, [navigate]);
 
   return (
     <SelectedIssuesProvider>
